690cef028d3747818200a3708084bb94eafaa190
Andoryuu <chlebaa@email.cz>
2022-12-10 17:35:45 +0100

    Improve day 1 solution

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use itertools::Itertools;
+
 fn main() {
     let input = fs::read_to_string("./_data/input.txt").expect("oh noes");
 
@@ -13,48 +15,23 @@ fn main() {
 fn process_data(input: String) -> String {
     input
         .lines()
-        .fold((0, Option::<u32>::None), |t, line| {
-            let new = line.trim().parse::<u32>().unwrap();
-
-            let new_count =
-                t.1.map(|old| if old < new { t.0 + 1 } else { t.0 })
-                    .unwrap_or(0);
-
-            (new_count, Some(new))
-        })
-        .0
+        .map(|line| line.trim().parse::<u32>().unwrap())
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
         .to_string()
 }
 
 fn process_data_adv(input: String) -> String {
     input
         .lines()
-        .fold(SlidingTotal::default(), |t, line| {
-            let new = line.trim().parse::<u32>().unwrap();
-
-            let new_count = t
-                .oldest
-                .and_then(|v| t.older.map(|iv| v < (iv + new)))
-                .map(|b| if b { t.count + 1 } else { t.count })
-                .unwrap_or(0);
-
-            SlidingTotal {
-                count: new_count,
-                oldest: t.older.map(|v| v + new),
-                older: t.old.map(|v| v + new),
-                old: Some(new),
-            }
-        })
-        .count
+        .map(|line| line.trim().parse::<u32>().unwrap())
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
         .to_string()
-}
-
-#[derive(Default)]
-struct SlidingTotal {
-    count: i32,
-    old: Option<u32>,
-    older: Option<u32>,
-    oldest: Option<u32>,
 }
 
 #[cfg(test)]
