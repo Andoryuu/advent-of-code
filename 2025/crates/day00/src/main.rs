a38b7d42c493670c7b331cf9d695b9d807bfcff7
Andoryuu <chlebaa@email.cz>
2025-12-01 09:19:57 +0100

    Prepare 2025

---
+++ 2025/crates/day00/src/main.rs
@@ -0,0 +1,80 @@
+#![feature(test)]
+
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./crates/day00/_data/input.txt").expect("oh noes");
+
+    println!("Part 1 result is: {}", process_part_1(&input));
+    println!("Part 2 result is: {}", process_part_2(&input));
+}
+
+fn process_part_1(input: &str) -> String {
+    "".to_owned()
+}
+
+fn process_part_2(input: &str) -> String {
+    "".to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+
+    use crate::*;
+    use rstest::{fixture, rstest};
+    use test::Bencher;
+
+    const TEST_CASE: &str = "";
+
+    #[fixture]
+    fn input() -> String {
+        fs::read_to_string("./_data/input.txt").expect("oh noes")
+    }
+
+    #[rstest]
+    #[case(TEST_CASE, "")]
+    fn part_1_check(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(expected, process_part_1(input));
+    }
+
+    #[rstest]
+    #[case(TEST_CASE, "")]
+    fn part_2_check(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(expected, process_part_2(input));
+    }
+
+    #[rstest]
+    #[case("")]
+    fn part_1_control(input: String, #[case] expected: &str) {
+        assert_eq!(expected, process_part_1(&input));
+    }
+
+    #[rstest]
+    #[case("")]
+    fn part_2_control(input: String, #[case] expected: &str) {
+        assert_eq!(expected, process_part_2(&input));
+    }
+
+    #[bench]
+    fn part_1_check_bench(b: &mut Bencher) {
+        b.iter(|| process_part_1(TEST_CASE));
+    }
+
+    #[bench]
+    fn part_1_control_bench(b: &mut Bencher) {
+        let input = input();
+        b.iter(|| process_part_1(&input));
+    }
+
+    #[bench]
+    fn part_2_check_bench(b: &mut Bencher) {
+        b.iter(|| process_part_2(TEST_CASE));
+    }
+
+    #[bench]
+    fn part_2_control_bench(b: &mut Bencher) {
+        let input = input();
+        b.iter(|| process_part_2(&input));
+    }
+}
